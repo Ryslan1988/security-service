c170ba3cab94d9a87c8de8769d75ed4b3c6e26f7
1 <a1@MacBook-Pro-1.local>
2026-01-28 19:49:57 +0300

    Initial commit

---
+++ src/test/java/by/javaguru/jdmk13/securityservice/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.javaguru.jdmk13.securityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
