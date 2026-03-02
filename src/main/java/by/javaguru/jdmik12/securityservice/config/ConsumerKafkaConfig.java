f1523a18db033b5d9a1ce2f9d763d97926858aee
1 <a1@MacBook-Pro-1.local>
2026-03-02 18:14:51 +0300

    JDMK12-159 add dlt

--- src/main/java/by/javaguru/jdmik12/securityservice/config/ConsumerKafkaConfig.java
+++ src/main/java/by/javaguru/jdmik12/securityservice/config/ConsumerKafkaConfig.java
@@ -1,17 +1,22 @@
 package by.javaguru.jdmik12.securityservice.config;
 
+import by.javaguru.jdmik12.common.base.KafkaMessage;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import jakarta.validation.ValidationException;
 import org.apache.commons.lang3.SerializationException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -21,6 +26,8 @@ import java.nio.file.AccessDeniedException;
 
 @Configuration
 public class ConsumerKafkaConfig {
+    @Value("${integration.kafka.producer.dlt.topic.name}")
+    private String dltTopic;
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
@@ -53,9 +60,15 @@ public class ConsumerKafkaConfig {
     }
 
     @Bean
-    public DefaultErrorHandler errorHandler() {
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, KafkaMessage> kafkaTemplate) {
         FixedBackOff backOff = new FixedBackOff(1000L, 3);
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(backOff);
+
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+                kafkaTemplate,
+                (record, exception) -> new TopicPartition(dltTopic, record.partition()));
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, backOff);
+
         errorHandler.addNotRetryableExceptions(IllegalArgumentException.class, IllegalStateException.class,
                 ValidationException.class, JsonParseException.class, JsonMappingException.class, SerializationException.class,
                 AccessDeniedException.class);
